7870b3150c2a23644526b02e3546a188c8fcadf9
Simon Núñez Aschenbrenner <68073627+simonaschenbrenner@users.noreply.github.com>
2024-05-03 18:35:45 +0200

    Initial Commit

---
+++ bleep-eval/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bleep-eval
+//
+//  Created by Simon Núñez Aschenbrenner on 03.05.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Simon!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bleep-eval/bleep_evalApp.swift
@@ -0,0 +1,17 @@
+//
+//  bleep_evalApp.swift
+//  bleep-eval
+//
+//  Created by Simon Núñez Aschenbrenner on 03.05.24.
+//
+
+import SwiftUI
+
+@main
+struct bleep_evalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
